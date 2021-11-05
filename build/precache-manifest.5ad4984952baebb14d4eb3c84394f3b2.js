a358112357a53e75ab17d8667ef1e3baf543d9b3
njosh92 <njosh92@gmail.com>
2021-11-05 13:39:57 -0600

    deploy

---
+++ build/precache-manifest.5ad4984952baebb14d4eb3c84394f3b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93037619b4dc5f526eb9b379a93416d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62bb76195e4e1e66b2eb",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "45f74ffa26dcb807a537",
+    "url": "/static/css/main.08b15357.chunk.css"
+  },
+  {
+    "revision": "62bb76195e4e1e66b2eb",
+    "url": "/static/js/2.ed861d6f.chunk.js"
+  },
+  {
+    "revision": "77a46977c9af9c7c94eaa45ca002a101",
+    "url": "/static/js/2.ed861d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f74ffa26dcb807a537",
+    "url": "/static/js/main.d2088273.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
